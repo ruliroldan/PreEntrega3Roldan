b4754dc9c983f2cd295dac2caa1d616b917822ad
ruli.roldan <ruliroldan@gmail.com>
2023-08-30 11:46:44 -0300

    Archivo index.html y archivo index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+const calculateTotalTime = (hours, minutes, seconds) => hours * 3600 + minutes * 60 + seconds;
+
+const calculateAveragePace = (totalTimeSeconds, distance) => {
+    if (totalTimeSeconds <= 0 || distance <= 0) {
+        return "El tiempo y la distancia deben ser mayores que 0.";
+    }
+
+    const averagePaceSecondsPerKm = totalTimeSeconds / distance;
+    return averagePaceSecondsPerKm;
+};
+
+const formatTime = seconds => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    return `${hours} horas, ${minutes} minutos y ${remainingSeconds} segundos`;
+};
+
+const main = () => {
+    const distance = parseFloat(prompt("Ingresa la distancia en kilómetros"));
+    console.log(`Distancia ingresada: ${distance} kilómetros`);
+
+    const timeInput = prompt("Ingresa el tiempo en formato 'hh:mm:ss':");
+    const timeParts = timeInput.split(":");
+    const hours = parseFloat(timeParts[0]);
+    const minutes = parseFloat(timeParts[1]);
+    const seconds = parseFloat(timeParts[2]);
+    console.log(`Tiempo ingresado: ${hours} horas, ${minutes} minutos, ${seconds} segundos`);
+
+    const totalTimeSeconds = calculateTotalTime(hours, minutes, seconds);
+    console.log(`Tiempo total en segundos: ${totalTimeSeconds} segundos`);
+
+    const averagePace = calculateAveragePace(totalTimeSeconds, distance);
+    const formattedTime = formatTime(Math.round(averagePace));
+    console.log(`Tu promedio es de ${formattedTime} por kilómetro.`);
+
+    let message = `Tu promedio es de ${formattedTime} por kilómetro.`;
+
+    switch (true) {
+        case averagePace > 600: // Más de 10 minutos por kilómetro
+            message += "\n¡Vamos, puedes mejorar tu ritmo!";
+            break;
+        case averagePace <= 360: // Menos de 6 minutos por kilómetro
+            message += "\n¡Excelente ritmo!";
+            break;
+        default:
+            message += "\nSigue entrenando para mejorar tu tiempo.";
+    }
+
+    console.log(message);
+    alert(message);
+};
+
+main();
