68fe9528d2f3cc7c4ba18bd7767522491e9cd83f
Ruli.Roldan <rulroldan@gmail.com>
2023-10-12 12:44:01 -0300

    Agregando cambios en index.js y el archivo data.json

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-const calculateTotalTime = (hours, minutes, seconds) => hours * 3600 + minutes * 60 + seconds;
-
-const calculateAveragePace = (totalTimeSeconds, distance) => {
-    if (totalTimeSeconds <= 0 || distance <= 0) {
-        return "El tiempo y la distancia deben ser mayores que 0.";
-    }
-
-    const averagePaceSecondsPerKm = totalTimeSeconds / distance;
-    return averagePaceSecondsPerKm;
-};
-
-const formatTime = seconds => {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const remainingSeconds = seconds % 60;
-
-    return `${hours}:${minutes}:${remainingSeconds}`;
-};
-
 document.addEventListener("DOMContentLoaded", function () {
     const calculateButton = document.getElementById("calculateButton");
     const resultDiv = document.getElementById("result");
+    const dailyAveragesDiv = document.getElementById("dailyAverages");
+
+    let dailyAverages = [];
+
+    function loadJSONData() {
+        fetch("data.json")
+            .then(response => response.json())
+            .then(data => {
+                dailyAverages = data;
+                showDailyAverages();
+            })
+            .catch(error => console.error(error));
+    }
+
+    loadJSONData();
 
     calculateButton.addEventListener("click", function () {
         const distanceInput = document.getElementById("distanceInput").value;
         const timeInput = document.getElementById("timeInput").value;
+
+        if (distanceInput === "" || timeInput === "") {
+            resultDiv.innerHTML = "Por favor, completa ambos campos.";
+            return;
+        }
 
         const distance = parseFloat(distanceInput);
         const timeParts = timeInput.split(":");
         const hours = parseFloat(timeParts[0]);
         const minutes = parseFloat(timeParts[1]);
         const seconds = parseFloat(timeParts[2]);
+
+        if (isNaN(distance) || isNaN(hours) || isNaN(minutes) || isNaN(seconds)) {
+            resultDiv.innerHTML = "Por favor, ingresa datos válidos.";
+            return;
+        }
 
         const totalTimeSeconds = calculateTotalTime(hours, minutes, seconds);
         const averagePace = calculateAveragePace(totalTimeSeconds, distance);
@@ -39,17 +45,43 @@ document.addEventListener("DOMContentLoaded", function () {
             ? "¡Vamos, puedes mejorar tu ritmo!"
             : (averagePace <= 360 ? "¡Excelente ritmo!" : "Sigue entrenando para mejorar tu tiempo.");
 
-        resultDiv.innerHTML = ""; // Limpia cualquier contenido anterior
+        resultDiv.innerHTML = "";
         const resultParagraph = document.createElement("p");
         resultParagraph.textContent = `Tu promedio es de ${formattedTime} por kilómetro.`;
         resultDiv.appendChild(resultParagraph);
 
-        localStorage.setItem("averagePace", averagePace);
+        dailyAverages.push({
+            day: new Date().toLocaleDateString(),
+            averagePace: averagePace
+        });
+
+        showDailyAverages();
+        localStorage.setItem("dailyAverages", JSON.stringify(dailyAverages));
+        saveJSONData();
     });
 
-    const savedAveragePace = localStorage.getItem("averagePace");
-    if (savedAveragePace) {
-        const formattedTime = formatTime(Math.round(parseFloat(savedAveragePace)));
-        resultDiv.innerHTML = `<p>Tu promedio anterior era de ${formattedTime} por kilómetro.</p>`;
+    function showDailyAverages() {
+        dailyAveragesDiv.innerHTML = "";
+
+        dailyAverages.forEach((data, index) => {
+            const day = data.day;
+            const average = data.averagePace;
+            const paragraph = document.createElement("p");
+            paragraph.textContent = `Día ${day}: ${formatTime(Math.round(average))} por kilómetro.`;
+            dailyAveragesDiv.appendChild(paragraph);
+        });
+    }
+
+    function saveJSONData() {
+        fetch("data.json", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dailyAverages)
+        })
+            .then(response => response.json())
+            .then(data => console.log("Datos guardados en data.json:", data))
+            .catch(error => console.error("Error al guardar los datos:", error));
     }
 });
